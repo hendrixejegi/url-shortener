3506405fdc2e9b9192f4c09aa6d32b28c8ebd1c3
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-12-13 00:44:26 +0100

    Write to database

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
 const express = require('express');
-const fs = require('node:fs/promises');
+const { randomUUID } = require('node:crypto');
+const { readFile, writeFile } = require('node:fs/promises');
 const path = require('node:path');
 
 const app = express();
 const DBPath = './db.json';
 
-// Helper function to read DB
-async function readDB(location) {
+/**
+ * Helper function to read DB
+ * @returns {Promise<{db: {id:string; originalURL: string; shortenedURL: string}[]}>}
+ */
+async function readDB() {
   try {
-    const filePath = path.resolve(location);
-    const contents = await fs.readFile(filePath, { encoding: 'utf-8' });
+    const filePath = path.resolve(DBPath);
+    const contents = await readFile(filePath, { encoding: 'utf-8' });
     console.log('Data read successfully');
     return JSON.parse(contents);
-  } catch (err) {
-    console.error(err.message);
+  } catch (error) {
+    console.error(error.message);
     return null;
   }
 }
+
+/**
+ *
+ * @param {{id:string; originalURL: string; shortenedURL: string}} data
+ */
+async function addLink(data) {
+  const contents = await readDB();
+  contents.db.push(data);
+  await writeDB(contents);
+}
+
+/**
+ * Helper function to write to DB
+ * @param {{db: {id:string; originalURL: string; shortenedURL: string}[]}} data
+ */
+async function writeDB(data) {
+  try {
+    const jsonString = JSON.stringify(data, null, 2);
+    const filePath = path.resolve(DBPath);
+    await writeFile(filePath, jsonString, { encoding: 'utf-8' });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Helper function to generate random URL
+function generateURL(length) {
+  var result = '';
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// addLink({
+//   id: randomUUID(),
+//   originalURL: 'https://google.com',
+//   shortenedURL: generateURL(5),
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello world');
