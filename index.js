058535ad0024bd9ea81f4d7ed241a809d1084746
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-12-12 21:34:14 +0100

    Read database

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
 const app = express();
+const DBPath = './db.json';
 
-const port = 3000;
+// Helper function to read DB
+async function readDB(location) {
+  try {
+    const filePath = path.resolve(location);
+    const contents = await fs.readFile(filePath, { encoding: 'utf-8' });
+    console.log('Data read successfully');
+    return JSON.parse(contents);
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(port, () => {
-  console.log(`server is listening at ${port}...`);
-});
+const port = 3000;
+
+const start = async () => {
+  const data = await readDB(DBPath);
+
+  app.listen(port, () => {
+    console.log(`server is listening at ${port}...`);
+  });
+};
+
+start();
